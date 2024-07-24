c6221d78cb1411bf86400e6cc9a622d65601a753
Paul Bjerk <51205318+paulbjerk@users.noreply.github.com>
2024-07-23 23:30:33 -0500

    Update setup.py
    added folder creation

--- setup.py
+++ setup.py
@@ -4,16 +4,19 @@ import os
 print("\nCopyright (c) <2024>, <Paul Bjerk>")
 print("All rights reserved.")
 print("\nThis source code is licensed under the BSD2-style license found at https://opensource.org/license/bsd-2-clause .")
+# https://www.reddit.com/r/LangChain/comments/15a447w/chroma_or_faiss/
+# https://python.langchain.com/v0.2/docs/integrations/vectorstores/chroma/
 
 embed_model = "mxbai-embed-large"
 embed_model_dimensions = "1024"
 embed_model_layers = "24"
-basic_inference_model = "phi3:3.8b-mini-128k-instruct-q5_K_M"
+#basic_inference_model = "phi3:3.8b-mini-128k-instruct-q5_K_M"
 inference_model = "phi3:3.8b-mini-128k-instruct-q5_K_M"
-inference_model_short = "phi3"
+large_inference_model = "llama3:instruct"
+inference_model_short, inference_model_detail = inference_model.split(":")
 userresponse = ""
 
-print("This setup process will take 5-10 minutes. You will need a 2020 or later Apple Mac with an M-Series chip. \nIt will install all the needed pieces (dependencies) for running the two apps.\n Before running this, you must first install Ollama from https://ollama.com \n ")
+print("This setup process will take 10-15 minutes. You will need a 2020 or later Apple Mac with an M-Series chip. \nIt will install all the needed pieces (dependencies) for running the two apps.\n Before running this, you must first install Ollama from https://ollama.com \n ")
 print("This will install ChromaDB first, and then install two AI language models from Ollama.\n"
       "First install Ollama and move the ai-assistant folder into your main user folder, \nthen run this set up app in the Mac Terminal by typing:\n cd ai-assistant \n python3 setup.py \n ")
 
@@ -28,10 +31,10 @@ mac_model = input ("What chip does your Mac have? Type M1 or M2, or M3: ")
 ram_memory_input = input ("How many GB of RAM memory does your Mac have? (type a number): ")
 ram_memory = int(ram_memory_input)
 
-
 archive_collection = input("What is the broadest one-word category for these documents? (e.g. the archive abbreviation, or collection name): ")
 topic_collection = input("If this is a large collection, what is the sub-set of these documents (e.g. the relevant country, theme, topic, individual, case): ")
 print("You should place the CSVs in nested folders in the ai-assistant folder, the nested folders should follow the one-word broad category and sub-set names entered above")
+
 
 #install chromadb and the underlying chat model (LLM: phi3)
 print("Make sure the above information was entered correctly. \nThis next step will run automatically and take a 5-10 minutes depending on system and internet speed")
@@ -49,8 +52,8 @@ os.system("pip3 install ollama")
 print("Ollama has been installed. For more information see https://ollama.com \n")
 os.system("ollama pull "+ embed_model)
 print("The embedding model, "+embed_model+" has been installed. \nFor more information see https://ollama.com/blog/embedding-models \n")
-os.system("ollama pull " + basic_inference_model)
-print("The basic LLM inference model, "+basic_inference_model+" has been installed. \nFor more information see https://ollama.com/library/phi3 \n")
+os.system("ollama pull " + inference_model)
+print("The basic LLM inference model, "+inference_model+" has been installed. \nFor more information see https://ollama.com/library/"+inference_model_short+".\n")
 
 # functions used
 
@@ -60,6 +63,11 @@ def create_csv(collection):
         writer = csv.DictWriter(new_file, fieldnames=fieldnames)
         writer.writeheader()
 
+def create_folder(archive_collection):
+    os.system("mkdir "+archive_collection)
+
+def create_sub_folder(archive_collection, topic_collection):
+    os.system("mkdir "+archive_collection+"/"+topic_collection)
 
 # These create models (modelfile) with context lengths appropriate to the user's RAM memory
 #https://github.com/ollama/ollama/blob/main/docs/modelfile.md
@@ -125,8 +133,10 @@ PARAMETER num_ctx 12288
 PARAMETER repeat_last_n 1536""")
 
 def create_max_model_template ():
+    os.system("ollama pull " + large_inference_model)
+    print("The basic LLM inference model, " + large_inference_model + " has been installed. \nFor more information see https://ollama.com/library/" + inference_model_short + ".\n")
     with open("model-template.txt", "w") as file:
-        file.write("""FROM phi3:3.8b-mini-128k-instruct-q5_K_M
+        file.write("""FROM llama3:instruct
 TEMPLATE "{{ if .System }}<|system|>
 {{ .System }}<|end|>
 {{ end }}{{ if .Prompt }}<|user|>
@@ -192,20 +202,27 @@ elif ram_memory > 29:
         os.remove("model-template.txt")
         create_max_model_template()
         inference_model_window = "16k tokens"
-        os.system("ollama create phi3-16k -f model-template.txt")
-        os.system("ollama show --modelfile phi3-16k")
+        os.system("ollama create llama3-16k -f model-template.txt")
+        os.system("ollama show --modelfile llama3-16k")
     else:
         create_max_model_template()
         inference_model_window = "16k tokens"
-        os.system("ollama create phi3-16k -f model-template.txt")
-        os.system("ollama show --modelfile phi3-16k")
+        os.system("ollama create llama3-16k -f model-template.txt")
+        os.system("ollama show --modelfile llama3-16k")
 
 
 #these create starting points for collection CSVs
 create_csv(archive_collection)
 create_csv(topic_collection)
+create_folder(archive_collection)
+create_sub_folder(archive_collection, topic_collection)
 
-print("The information above summarizes the inference model we will use to explore the retrieved documents.\n The model allows a context length of " +inference_model_window+ ", which represent words or parts of words. \nDivide tokens by 1.5 to get approximate number of words that the model can analyze in the retrieved documents.\nEach page of these documents contains an average of 350 words, so a 2k model analyzes about 4 pages at a time, and an 8k model about 16 pages.")
+print("The information above summarizes the inference model we will use to explore the retrieved documents.\n The model allows a context length of " +inference_model_window+ ", which represent words or parts of words. \nDivide tokens by 1.5 to get approximate number of words that the model can analyze in the retrieved documents.\nEach page of these documents contains an average of 350 words, so a 2k model analyzes about 4 pages at a time, and an 8k model about 16 pages.\n")
+
+print("A folder called "+topic_collection+"has been created inside a folder called "+archive_collection+" in the ai-assistant folder.\nYou should copy CSV files to be ingested here.\n")
+print("A CSV template called all-"+archive_collection+"-documents.csv has been created.\nIngested documents will be added here together with all documents from this broad collection.\n")
+print("A CSV template called all-"+topic_collection+"-documents.csv has been created.\nIngested documents will also be added here together with all documents from this topic sub-set collection.\n")
+
 print("Now type python3 ingest.py in order to begin ingesting CSV documents.")
 
 #this removes a temporary file created for the purpose of creating the modelfile
