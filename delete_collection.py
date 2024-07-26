08c5cb06c7b22a357d48071ad265c616f0d7686e
Paul Bjerk <51205318+paulbjerk@users.noreply.github.com>
2024-07-25 23:44:19 -0500

    Update delete_collection.py
    improved it, this is still a work in progress.

--- delete_collection.py
+++ delete_collection.py
@@ -17,12 +17,15 @@ def delete_documents(collection):
 client = chromadb.PersistentClient(path="chromadb/phototextvectors")
 
 currentingest = input("What CSV file do you want to delete? (enter the filename only, without the .csv suffix) It's best to copy-paste to avoid typos ")
-sub_collection = input("What sub-collection does this belong to? \n These records will be removed from the sub-collection.")
-topic_collection = input("What overall collection does this belong to? \n These records will be removed from the overall collection.")
-
-print(collection.peek()) # returns a list of the first 10 items in the collection
+archive_collection = input("What archive collection does this belong to? \n These records will be removed from the archive collection.")
+archive_collection = str("all-"+archive_collection+"-documents")
+#print(archive_collection.peek()) # returns a list of the first 10 items in the collection
+topic_collection = input("What topic collection does this belong to? \n These records will be removed from the topic collection.")
+topic_collection = str("all-"+topic_collection+"-documents")
+#print(topic_collection.peek()) # returns a list of the first 10 items in the collection
 user_auth = input("Is this the collection you wish to delete? Type y or n: ")
-#collection.count() # returns the number of items in the collection
+#archive_collection.count() # returns the number of items in the collection
+#topic_collection.count() # returns the number of items in the collection
 #collection.modify(name="new_name") # Rename the collection
 
 #"Chroma supports deleting items from a collection by id using .delete. \n "
@@ -40,20 +43,22 @@ if user_auth == "y":
     #collection = client.get_or_create_collection(name="test") # Get a collection object from an existing collection, by name. If it doesn't exist, create it.
     #collection = client.get_collection(name=currentingest)
     client.delete_collection(name=currentingest) # Delete a collection and all associated embeddings, documents, and metadata. ⚠️ This is destructive and not reversible
+    client.delete_collection(name=archive_collection) # Delete a collection and all associated embeddings, documents, and metadata. ⚠️ This is destructive and not reversible
+    client.delete_collection(name=topic_collection) # Delete a collection and all associated embeddings, documents, and metadata. ⚠️ This is destructive and not reversible
 
-    collection = client.get_collection(name=str("all-"+ sub_collection +"-documents"))
-    collection.delete(
-        ids=["id1", "id2", "id3",...],
-        where={"FOLDERNAME": currentingest}
-    )
+    #collection = client.get_collection(name=str("all-"+ archive_collection +"-documents"))
+    #collection.delete(
+        #ids=["id1", "id2", "id3",...],
+        #where={"FOLDERNAME": currentingest}
+    #)
 
-    collection = client.get_collection(name=str("all-" + topic_collection + "-documents"))
-    collection.delete(
-        ids=["id1", "id2", "id3", ...],
-        where={"FOLDERNAME": currentingest}
-    )
+    #collection = client.get_collection(name=str("all-" + topic_collection + "-documents"))
+    #collection.delete(
+        #ids=["id1", "id2", "id3", ...],
+        #where={"FOLDERNAME": currentingest}
+    #)
 
-    #delete_documents(sub_collection)
+    #delete_documents(archive_collection)
     #delete_documents(topic_collection)
 else:
     print("We won't delete anything for now. \nIf you want to try again type python3 delete.collecction.py again in the Terminal.")
