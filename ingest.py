7fa16b38c0c3d1137d682cdb81f29683d0934d22
Paul Bjerk <51205318+paulbjerk@users.noreply.github.com>
2024-07-29 15:52:10 -0500

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -9,7 +9,7 @@ import statistics
 from statistics import mode
 
 print("\n   - - The Airqiv Document Explorer  - -       ")
-print("\n          - - www.airqiv.com  - -       ")
+print("          - - www.airqiv.com  - -       ")
 print("\nAI-Assistant Document Explorer")
 print("Copyright (c) <2024>, <Paul Bjerk>")
 print("All rights reserved.")
